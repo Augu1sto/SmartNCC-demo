1ff547ba878d33898c833d72659993ff9a7e1341
augu1sto <yingu081@163.com>
2022-08-30 22:55:38 +0800

    修改attendance mock逻辑；添加图标

--- mock/attendance.js
+++ mock/attendance.js
@@ -7,22 +7,42 @@ const Random = Mock.Random
 const time = require('@/utils/mytime.js');
 
 // 构造出所有的数据
+function getDetail(date) {
+	return Mock.mock({
+		"detail": {
+		    "isValid": '@boolean' , // 暂时搞成随机的
+		    "timeList|1-6": [{
+		        "time": function(){
+					let now = Random.now('HH:mm:ss');
+					let time = Random.time('HH:mm:ss');
+					if(Random.now('yyyy-MM-dd') !== date) {
+						return time;
+					}
+					// console.log(now);
+					while (time > now) {
+						time = Random.time('HH:mm:ss');
+					}
+					return time;
+				},
+		        'location': /(武大[A-C][1-4][0-1][1-9])|([1-3]号宿舍楼[2-8]层[1-8]单元电梯厅(左|右))/,
+		        "isValid": function() {
+					if(this.location[0]==="武") {
+						return true;
+					} else {
+						return false;
+					}
+				}
+		    }]
+		}
+	}).detail;
+}  
+
+
 const checkDataAll_origin = Mock.mock({"data|30":[{
     "id|+1":0, 
     "date": function(){return time.calcDate(-1*this.id)},
-    "detail": {
-        "isValid": '@boolean' , // 暂时搞成随机的
-        "timeList|1-6": [{
-            "time|+1": '@time',
-            'location': /(武大[A-C][1-4][0-1][1-9])|([1-3]号宿舍楼[2-8]层[1-8]单元电梯厅(左|右))/,
-            "isValid": function() {
-				if(this.location[0]==="武") {
-					return true;
-				} else {
-					return false;
-				}
-			}
-        }]
+    "detail": function(){
+		return getDetail(this.date);
     }
 }]}).data;
 
@@ -44,4 +64,4 @@ checkDataAll_origin.forEach((item)=>{
 const checkToday = checkDataAll[time.toDayDate()]
 // console.log(JSON.stringify(checkDataAll));
 
-export default {checkDataAll, validDays, checkToday};
+module.exports = {checkDataAll, validDays, checkToday};
